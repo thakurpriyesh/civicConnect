f6f8a013902370c0ed286d4c7143e2e81563710f
thakurpriyesh <thakurpriyesh01@gmail.com>
2026-05-25 00:02:43 +0530

    Fixed cors issue

--- server/server.js
+++ server/server.js
@@ -11,7 +11,11 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
